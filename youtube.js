da8d61f4a67fbe082c45da6fe5329306003e21c1
Riomarre <alhassan.choucair@gmail.com>
2022-04-29 04:21:06 +0200

    number 1

---
+++ youtube.js
@@ -0,0 +1,30 @@
+dt = new Date();
+
+document.getElementById("date").innerHTML= "The website was accessed on "+ dt;
+
+document.getElementById("yes").onclick= function(){
+
+	document.getElementById("ans1").innerHTML= "I wannt to kiss you!";
+	document.getElementById("yes").style.display = "none";
+	document.getElementById("no").style.display = "none";
+	document.getElementById("text").innerHTML = "I'm Glad";
+	document.getElementById("ans2").innerHTML = "I will make you proud daddy!";
+
+
+
+}
+
+document.getElementById("no").onclick= function(){
+
+	document.getElementById("ans1").innerHTML= "";
+	document.getElementById("yes").style.display = "none";
+	document.getElementById("no").style.display = "none";
+	document.getElementById("text").innerHTML = "Allah hates you!";
+	document.getElementById("ans2").innerHTML = "Mehdi hates you too!";
+
+
+
+}
+
+
+document.getElementById("summary").innerHTML = "Note that this program is still experimental and will grow slowly!";
