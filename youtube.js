a7caf5896614985fb4d3b82fd4cabacb9fe91d2e
Riomarre <alhassan.choucair@gmail.com>
2022-04-29 05:15:29 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,9 +1,9 @@
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin","*");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 next();
 });
-
+*/
 dt = new Date();
 
 
