fb759aa100f83f00a69ac5f3e70109c3747e5753
Riomarre <alhassan.choucair@gmail.com>
2022-05-02 06:06:34 +0200

    website 1

--- backend.js
+++ backend.js
@@ -2,5 +2,19 @@
 dt = new Date();
 
 
-document.getElementById("date").innerHTML= "The website was accessed on "+ dt;
+document.getElementById("end").innerHTML= "The website was accessed on "+ dt;
+
+document.getElementById("y1").onclick= function(){
+	document.getElementById("dick").innerHTML =" Correct!! But how did you know? \n real size to scale: ===>";
+	document.getElementById("y1").style.display = "none";
+	document.getElementById("y2").style.display = "none";
+
+}
+document.getElementById("y2").onclick= function(){
+	document.getElementById("dick").innerHTML ="Sure honey, keep dreaming \n real size to scale: ===>";
+	document.getElementById("y1").style.display = "none";
+	document.getElementById("y2").style.display = "none";
+
+}
+
 
