824ec2a3df7fc4c545734d7fed875d79aa579232
Riomarre <alhassan.choucair@gmail.com>
2022-09-24 17:54:18 +0200

    login page added
    yeyyymee

---
+++ login.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "potato" && password === "labneh123") {
+        alert("You have successfully logged in.");
+        window.location.replace("mainpage.html");
+    } else {
+        loginErrorMsg.style.opacity = 1;
+
+    }
+})
