8a659528942465af095e2d3d08f254ab607aa723
Riomarre <alhassan.choucair@gmail.com>
2022-10-01 14:18:45 +0200

    Update login.js

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "potato" && password === "labneh123") {
+    if (username.toLowerCase() === "potato" && password === "labneh123") {
         alert("You have successfully logged in.");
         window.location.replace("mainpage.html");
     } else {
