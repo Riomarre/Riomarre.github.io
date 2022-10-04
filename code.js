2da1bb463c3f622a7f08c924847b29e155368b4a
Riomarre <alhassan.choucair@gmail.com>
2022-10-04 12:42:26 +0200

    register system missing fxn
    ss

--- code.js
+++ code.js
@@ -1,3 +1,104 @@
-var x =69;
 
-dt = new Date();
+
+
+
+
+
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+const loginHeader = document.getElementById("login-header");
+
+const regButton = document.getElementById("reg-form-submit");
+const regForm = document.getElementById("registerForm");
+
+
+const regButton = document.getElementById("reg2-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username.toLowerCase() === "potato" && password === "labneh123") {
+        alert("You have successfully logged in.");
+        window.location.replace("mainpage.html");
+    } else {a
+        loginErrorMsg.style.opacity = 1;
+
+    }
+})
+
+
+//REGISTER BUTTON
+
+regButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    regForm.style.opacity =1;
+    loginForm.remove();
+    loginHeader.innerHTML = "Join our OnlyFriends Family Now!";  
+
+})
+
+
+
+/*
+            document.getElementById("reg2-form-submit").addEventListener("click", (e) => {
+             e.preventDefault();
+             const signup = document.getElementById("registerForm");
+             
+
+             fullName = signup.fullName.value;
+
+             email= signup.email.value;
+             document.getElementById("userData").innerHTML(fullName + "\n" + email);
+
+
+
+
+             window.location.replace("mainpage.html");
+
+  
+                })
+
+
+//SIGN-UP SECTION
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+document.getElementById("reg2-form-submit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const link = document.createElement("a");
+  link.download = 'test.txt';
+  link.href = window.URL.createObjectURL(
+    new Blob([ [
+      document.getElementById("fullName-field").value,
+      document.getElementById("newPass-field").value
+    ].join('\n') ],
+    { type: 'text/plain' })
+  );
+  link.style.display = "none";
+  document.body.appendChild(link);
+
+  link.click();
+  link.remove();
+});
+
+// DOWNLOAD TXT doc
+
+*/
--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+const loginHeader = document.getElementById("login-header");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -15,3 +16,33 @@ loginButton.addEventListener("click", (e) => {
 
     }
 })
+
+const regButton = document.getElementById("reg-form-submit");
+const regForm = document.getElementById("registerForm");
+
+regButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginErrorMsg.style.opacity = 0;
+    regForm.style.opacity =1;
+    loginForm.remove();
+    loginHeader.innerHTML = "Join our OnlyFriends Family Now!";  
+
+})
+
+
+const regButton2 = document.getElementById("reg2-form-submit");
+const userInfo =document.getElementById("userData");
+
+regButton2.addEventListener("click", (e) => {
+             e.preventDefault();
+
+             const name = document.getElementById('fullName-field').value;
+               // document.write("value is "+ name);
+
+
+             window.location.replace("mainpage.html");
+
+  
+                })
+
+
