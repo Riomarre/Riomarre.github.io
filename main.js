8feae8dc73b1efbc231ec8efecd554754b80da52
Riomarre <alhassan.choucair@gmail.com>
2022-11-01 22:04:22 +0100

    Italian Translation

---
+++ main.js
@@ -0,0 +1,34 @@
+var currentyear = new Date().getFullYear();
+
+
+
+function updyear(){
+	document.getElementById("year").innerHTML = currentyear;
+};
+
+function transIt(){
+	document.getElementById("txt1").innerHTML = It1;
+	document.getElementById("txt2").innerHTML = "Eventi";
+	document.getElementById("txt3").innerHTML = "Conoscerci di più";
+	document.getElementById("AEGEE").innerHTML = "Cos’è AEGEE?";
+	document.getElementById("txt5").innerHTML = It5;
+	document.getElementById("purpose").innerHTML = "Scopo";
+	document.getElementById("txt6").innerHTML = It6;
+	document.getElementById("member").innerHTML="Vantaggi per i membri";
+	document.getElementById("txt7").innerHTML = It7;
+	document.getElementById("sp1").innerHTML = "Saliamo";
+	document.getElementById("sp2").innerHTML = "i";
+	document.getElementById("sp3").innerHTML = "confini";
+
+};
+
+
+
+
+
+
+//translation objects here
+let It1 = '<h2> Chi siamo?</h2><p>Benvenuto nella pagina di AEGEE-Torino! Siamo molto felici di farci controllare. <br><em>Allora, chi siamo?</em> <br>Siamo un gruppo di studenti/lavoratori il cui compito è aiutare la comunità internazionale a diventare parte del locale! Quindi, invece di esplorare Torino come un tour, puoi esplorarla come un locale. Ottieni tutti i fantastici luoghi segreti ed esplori la città da una prospettiva completamente unica. <br><em> Allora cosa stai aspettando? Unisciti a noi! </em></p>';
+let It5 = 'AEGEE-Torino è un\'associazione di volontariato fondata nel 1992 da un gruppo di studenti torinesi. È una delle filiali locali di AEGEE-Europe, una rete europea di associazioni studentesche interdisciplinari con oltre 15.000 membri e 217 sedi in tutta Europa. <br>A livello locale, AEGEE-Torino si impegna a diffondere i valori chiave della rete, come Sostenibilità, Parità di genere e Cittadinanza Europea.';
+let It6 ="Per raggiungere i nostri obiettivi, organizziamo conferenze, corsi, tavole rotonde, ricerche e altri eventi su questioni sociali europee come il multilinguismo, l\'allargamento e la diversità. Negli ultimi 4 anni più di 6000 giovani hanno partecipato a 40 diversi progetti locali e internazionali. Tutti i nostri progetti si concentrano sull\'integrazione degli strumenti educativi dell\'educazione formale con attività educative non formali e informali. I membri di AEGEE-Torino sono tutti giovani tra i 18 ei 35 anni che collaborano attivamente alla realizzazione dei principali progetti.";
+let It7 = 'I membri della nostra ONG sbloccano offerte esclusive che includono scambi convenienti con altre antenne.';
